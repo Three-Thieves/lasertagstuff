32485e37d03269df4a1729f38dcfa1bb6b90c19f
Frank Lee <nicc51591@live.com>
2023-08-03 01:56:24 -0400

    Add files via upload

---
+++ code/Main.cs
@@ -0,0 +1,17 @@
+﻿using TerrorTown;
+using Sandbox;
+
+namespace QZarMods
+{
+	public class QZarMods
+	{
+		[Event("Player.PostDress")]
+		public static void ApplyClothing(TerrorTown.Player ply)
+		{
+			var clothing = ResourceLibrary.Get<Clothing>("models/3t/qzar/vest/lasertagvest.clothing");
+			
+			ply.Clothing.Toggle(clothing);
+			ply.Clothing.DressEntity(ply);
+		}
+	}
+}
